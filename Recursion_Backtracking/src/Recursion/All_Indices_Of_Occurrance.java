caff2232a0d670e711a812e2491e321f5415c494
asmitapaul22 <asmitapaul2000@gmail.com>
2021-07-25 17:20:17 +0530

    checking two

--- Recursion_Backtracking/src/Recursion/All_Indices_Of_Occurrance.java
+++ Recursion_Backtracking/src/Recursion/All_Indices_Of_Occurrance.java
@@ -2,6 +2,6 @@ package Recursion;
 
 public class All_Indices_Of_Occurrance {
     public static void main(String[] args) {
-System.out.println("Checking");
+System.out.println("Checking two");
     }
 }

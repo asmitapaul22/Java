0bab189c6f15ee9b232c63fc52c91f9ab10ea94b
asmitapaul22 <asmitapaul2000@gmail.com>
2021-07-28 21:37:13 +0530

    get_subsequence added

--- Recursion_Backtracking/src/Recursion/All_Indices_Of_Occurrance.java
+++ Recursion_Backtracking/src/Recursion/All_Indices_Of_Occurrance.java
@@ -1,7 +1,11 @@
 package Recursion;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class All_Indices_Of_Occurrance {
     public static void main(String[] args) {
-System.out.println("Checking two");
+BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+//int n=
     }
 }
--- Recursion_Backtracking/src/Recursion/Get_Subsequence.java
+++ Recursion_Backtracking/src/Recursion/Get_Subsequence.java
@@ -1,4 +1,33 @@
 package Recursion;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Get_Subsequence {
+    public static void main(String[] args) {
+        Scanner scn =new Scanner(System.in);
+        String str=scn.next();
+        ArrayList<String> res=gss(str);
+        System.out.println(res);
+    }
+    public static ArrayList<String> gss(String str)
+    {
+        if(str.length()==0)
+        {
+            ArrayList<String> bres=new ArrayList<String>();
+            bres.add("");
+            return bres;
+        }
+        char ch=str.charAt(0);
+        String ros=str.substring(1);
+        ArrayList<String> rres=gss(ros);
+
+        ArrayList<String> mres=new ArrayList<String>();
+        for(String rstr:rres)
+            mres.add(""+rstr);
+        for(String rstr:rres)
+            mres.add(ch+rstr);
+        return mres;
+    }
 }
+

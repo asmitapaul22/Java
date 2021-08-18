23497dc4747228f84d2fd8a402ed5763e99607e1
asmitapaul22 <asmitapaul2000@gmail.com>
2021-08-18 14:24:28 +0530

    day-13

--- Java/src/Arrays_and_Strings_Level2/Sieve_Of_Eratosthenes_Prime_Generation_Algorithm.java
+++ Java/src/Arrays_and_Strings_Level2/Sieve_Of_Eratosthenes_Prime_Generation_Algorithm.java
@@ -1,7 +1,16 @@
 package Arrays_and_Strings_Level2;
 
+import java.util.Scanner;
+
 public class Sieve_Of_Eratosthenes_Prime_Generation_Algorithm {
     public static void main(String[] args) {
-
+        Scanner scn=new Scanner(System.in);
+        int n=scn.nextInt();
+        printPrimeUsingSieve(n);
+    }
+    public static void printPrimeUsingSieve(int n)
+    {
+        
     }
 }
+ 
